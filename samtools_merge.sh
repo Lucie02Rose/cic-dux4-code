1688f28875eb23cb7165d43f7960aa39de3249d5
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-26 13:57:01 +0100

    Delete samtools_merge.sh

--- samtools_merge.sh
+++
@@ -1,4 +0,0 @@
-module load samtools
-
-cd /lustre/scratch126/casm/team274sb/na15/PacBio/shells/ 
-samtools merge -r -b /lustre/scratch126/casm/team274sb/na15/PacBio/shells/bamlist -o /lustre/scratch126/casm/team274sb/na15/PacBio/CICSARC.merged.bam
