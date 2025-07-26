c89b74d86dcc7ce65fe2e1a9cd3f2cae515bd2ae
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-26 22:12:03 +0100

    Update allele-integrator-wrapper.sh

--- 2.sample_validation/allele-integrator-wrapper.sh
+++ 2.sample_validation/allele-integrator-wrapper.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # This is the AlleleIntegrator wrapper script (used by the group)
-CORES=52
-RAM="200G"
-QUEUE="hugemem"
-GROUP="team274"
+CORES=52 # no of cpus
+RAM="200G" # a lot of samples - a lot of memory
+QUEUE="hugemem" # big memory queue
+GROUP="team274" # group name
 
-SCRIPT="Rscript /nfs/users/nfs_l/lr26/allele_check/genotypeCheck.R "
+SCRIPT="Rscript /nfs/users/nfs_l/lr26/allele_check/genotypeCheck.R " # the wrapper runs this script
 # this is a shared conda environment within the group (pre-Sanger datacentre incident)
 conda activate alleleIntegrator
-
+# LSF uses bsub to submit jobs
 bsub \
 -G "${GROUP}" -q "${QUEUE}"  -n ${CORES} \
 -M ${RAM} -R "select[mem>${RAM}] rusage[mem=${RAM}]" \
