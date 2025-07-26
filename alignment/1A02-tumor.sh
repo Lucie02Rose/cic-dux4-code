6edee48530019e934e0e32b2739f16b8cbd5c044
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-26 10:54:20 +0100

    Update 1A02-tumor.sh

--- alignment/1A02-tumor.sh
+++ alignment/1A02-tumor.sh
@@ -15,7 +15,7 @@ conda activate base
 # Directories
 reference="/nfs/users/nfs_l/lr26/nextflow_pipeline/reference/T2T/chm13v2.0.fa"
 input_bam="/lustre/scratch126/cellgen/behjati/lr26/PacBio/tumor_1A02_hifi_reads.bam"
-output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-aligned/1A02"
+output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-aligned"
 tmp_dir="$output_dir/tmp"
 
 # Create temp directory if it doesn't exist
