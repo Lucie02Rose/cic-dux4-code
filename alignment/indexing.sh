8418d0f8f4fc5ad18c771a125f9c71e0e8aebf4d
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-26 11:00:13 +0100

    Update and rename 1B01_t2t_mm2.sh to alignment/indexing.sh

---
+++ alignment/indexing.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-index.o
+#BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-index.e
+#BSUB -n 16
+#BSUB -M 32000
+#BSUB -R 'span [hosts=1] select[mem>32000] rusage[mem=32000]'
+#BSUB -q normal 
+#BUSB -J indexing
+
+### activate my conda environment - pbcore is a separate and used for detailed metrics and qc
+source /software/cellgen/team274/lr26/miniforge3/bin/activate
+conda activate base
+
+### Directories to process
+reference_gz="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0.fa.gz"
+reference="/lustre/scratch126/cellgen/behjatib/lr26/T2T/chm13v2.0.fa"
+ref_dir="/lustre/scratch126/cellgen/behjati/lr26/T2T"
+
+cd "$ref_dir"
+
+# Decompress reference if not already done
+if [ ! -f "$reference" ]; then
+  gunzip -c "$reference_gz" > "$reference"
+fi
+
+# Indexing the T2T reference genome (if index files don't exist)
+if [ ! -f "$reference.mmi" ]; then
+  pbmm2 index "$reference"
+fi
