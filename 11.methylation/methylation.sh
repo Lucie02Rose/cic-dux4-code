1643d55ffed0170cba0a7030127b78d458ecf729
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-27 17:15:43 +0100

    Update methylation.sh

--- 11.methylation/methylation.sh
+++ 11.methylation/methylation.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
+### job specifications for the LSF ###
 #BSUB -n 16
 #BSUB -M 32000
 #BSUB -R 'span[hosts=1] select[mem>32000] rusage[mem=32000]'
 #BSUB -q long
-#BSUB -J methylation-giab-model
+#BSUB -J methylation
 #BSUB -G team274
 #BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-methylation.out
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-methylation.err
 
-### Activate conda environment
+### activate conda environment for methylation ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate methylation
 
-# Directories
-reference="/nfs/users/nfs_l/lr26/nextflow_pipeline/reference/T2T/chm13v2.0.mmi"
+### directories including the reference - which can be the mmi index here ###
+reference="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0.mmi"
 output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-methylation"
 input_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-aligned"
 
-# Create output directory
+### create the methylation output directory ###
 mkdir -p "$output_dir"
 
+### message for the output ###
 echo "Running methylation on all BAM files in $input_dir..."
 
-# Loop through all BAM files in the input directory and subdirectories
-for input_bam in "$input_dir"/*/*.bam; do
-    # Extract sample name from BAM filename
+### finding all files in the output that end with bam, retain their sample name
+for input_bam in "$input_dir"/*.bam; do
     sample_name=$(basename "$input_bam" .bam)
-
+    ### message - which sample is processed ###
     echo "Processing: $input_bam"
-
-    # Run methylation analysis
+    ### running the methylation with 16 threads in the for loop one sample after the other ###
     aligned_bam_to_cpg_scores --bam "$input_bam" --output-prefix  "$output_dir/${sample_name}-methylation" --threads 16
-
+    ### display message when file is done ###
     echo "Completed: $input_bam"
 done
-
+### display message when all is done ###
 echo "All methylation analyses completed."
 
 
