495be14c7fd29a5b52161d1ac3e580b230fceea5
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-27 19:39:44 +0100

    Update and rename t2t_indexing.sh to ndexing-t2t.sh

--- 3.alignment/ndexing-t2t.sh
+++ 3.alignment/ndexing-t2t.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 16
 #BSUB -M 32000
 #BSUB -R 'span [hosts=1] select[mem>32000] rusage[mem=32000]'
@@ -8,23 +9,23 @@
 #BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-index.o
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-index.e
 
-### activate my conda environment - pbcore is a separate and used for detailed metrics and qc
+### activate my conda environment - base has pbmm2 ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate base
 
-### Directories to process
-reference_gz="/lustre/scratch125/cellgen/behjati/lr26/T2T/chm13v2.0.fa.gz"
-reference="/lustre/scratch125/cellgen/behjati/lr26/T2T/chm13v2.0.fa"
-ref_dir="/lustre/scratch125/cellgen/behjati/lr26/T2T"
-
+### define the directories with the reference
+reference_gz="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0.fa.gz"
+reference="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0.fa"
+ref_dir="/lustre/scratch126/cellgen/behjati/lr26/T2T"
+### change to the reference directory ###
 cd "$ref_dir"
 
-# Decompress reference if not already done
+### decompress the reference if it is not yet done ###
 if [ ! -f "$reference" ]; then
   gunzip -c "$reference_gz" > "$reference"
 fi
 
-# Indexing the T2T reference genome (if index files don't exist)
+### index the genome to generate .mmi if not already done ###
 if [ ! -f "${reference%.fa}.mmi" ]; then
     pbmm2 index "$reference" "${reference%.fa}.mmi"
 fi
