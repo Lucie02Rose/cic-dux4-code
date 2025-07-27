48b3ac26606c55db9fcfef58cb9233b66e4b979c
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-27 12:41:04 +0100

    Update samtools-flagstat-lucie.sh

--- 3.alignment/QC/samtools-flagstat-lucie.sh
+++ 3.alignment/QC/samtools-flagstat-lucie.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+### define parameters for the LSF ###
 #BSUB -n 16
 #BSUB -M 50000
 #BSUB -R 'span[hosts=1] select[mem>50000] rusage[mem=50000]'
@@ -8,23 +9,25 @@
 #BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/%J.out
 #BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/%J.err
 
-### Activate Conda Environment
+### activate conda environment - samtools is in base ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate base
 
-### Directories
-
+### define the directories ###
 pbmm2_output="/lustre/scratch126/casm/team274sb/lr26/pbmm2-alignment"
 
-# Loop through all subdirectories
+### this searches for all aligned bam diles in the subdirectories on pbmm2-alignment
+### it loops through and checks if it is a directory or if there is a valid bam file
+### if it finds a bamfile then it defines a flagstat report incorporating the bam file name
+### runs a multithreaded samtools flagstat report which outputs some alignment metrics
 for sub_dir in "$pbmm2_output"/*; do
-    if [ -d "$sub_dir" ]; then  # Check if it's a directory
+    if [ -d "$sub_dir" ]; then 
         for bam_file in "$sub_dir"/*.bam; do
-            if [ -f "$bam_file" ]; then  # Check if it's a BAM file
-                # Define output flagstat report filename
+            if [ -f "$bam_file" ]; then  
+              
                 flagstat_report="${bam_file%.bam}.flagstat"
                 
-                # Run samtools flagstat
+ 
                 echo "Running flagstat for $bam_file..."
                 samtools flagstat -@ 16 "$bam_file" > "$flagstat_report"
                 
