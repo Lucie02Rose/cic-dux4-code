b9537bc8e0d1b531cec9e5c730ecb72f3028296f
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-27 12:23:52 +0100

    Update samtools-sn-pb.sh

--- 2.sample_validation/samtools-sn-pb.sh
+++ 2.sample_validation/samtools-sn-pb.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 16
-#BSUB -M 150000
-#BSUB -R 'span[hosts=1] select[mem>150000] rusage[mem=150000]'
+#BSUB -M 100000
+#BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
 #BSUB -q basement
 #BSUB -J improv_allele-bcftools
 #BSUB -G team274
 #BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/%J.out
 #BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/%J.err
 
-### Activate Conda Environment
+### conda environment activation (new versions of bcftools and samtools in base) ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate base
 
-### Directories
+### define directories for the reference, positions to be sampled, all samples ###
 reference="/lustre/scratch126/casm/team274sb/lr26/hg38/genome.fa"
 positions_bed="/lustre/scratch126/casm/team274sb/lr26/allele-integrator-pbfix/GenotypingResults/patient_output.bed"
 s1A01="/lustre/scratch126/casm/team274sb/lr26/pbmm2-alignment-team274sb-hg38/1_A01/m64094e_230126_154129.hifi_reads_pbmm2-farm22-bam.bam"
@@ -24,6 +25,12 @@ s1C01="/lustre/scratch126/casm/team274sb/lr26/pbmm2-alignment-team274-hg38-revio
 mom="/nfs/cancer_ref01/nst_links/live/3306/PD54859b/PD54859b.sample.dupmarked.bam"
 patient="/nfs/cancer_ref01/nst_links/live/3306/PD54858d/PD54858d.sample.dupmarked.bam"
 
+### for each of the samples defined above, calculate the number of nucleotides at the bed file position compared to reference
+### save allele depth information depending on the no of allele counts at the location, store in a bcf file, convert to vcf 
+### use awk to calculate total depth at each position and calculate alelle proportion dividing each by total
+### output the original and add the calculated allele frequency proportions to the new vcf
+
+### do this for all samples ###
 bcftools mpileup -f "$reference" -a FORMAT/AD -R "$positions_bed" -o s1A01.bcf "$s1A01"
 bcftools view s1A01.bcf -Ov -o s1A01.vcf
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t[%AD]\n' s1A01.vcf | \
