74fa3ca5b5fe5e25782ad803b0895b50b4f40940
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-27 19:24:42 +0100

    Update hg38-indexing-lucie.sh

--- 3.alignment/hg38-indexing-lucie.sh
+++ 3.alignment/hg38-indexing-lucie.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/%J.o
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/%J.e
+### parameters for the LSF job ###
 #BSUB -n 16
-#BSUB -M 100000
-#BSUB -R 'span [hosts=1] select[mem>100000] rusage[mem=100000]'
-#BSUB -q normal 
-#BSUB -J pbmm2indexing-hg38
+#BSUB -M 32000
+#BSUB -R 'span [hosts=1] select[mem>32000] rusage[mem=32000]'
+#BSUB -q normal
+#BUSB -J indexing
 #BSUB -G team274
+#BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-index.o
+#BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-index.e
 
-### activate my conda environment - pbcore is a separate and used for detailed metrics and qc
+### activate my conda environment base with pbmm2 ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate base
 
-### Directories to process
-reference="/lustre/scratch126/casm/team274sb/lr26/hg38/genome.fa"
-input_dir="/lustre/scratch126/casm/team274sb/lr26/hg38"
-output_dir="/lustre/scratch126/casm/team274sb/lr26/hg38"
+### directories ###
+reference_gz="/lustre/scratch126/cellgen/behjati/lr26/hg38/hg38.fa..gz"
+reference="/lustre/scratch126/cellgen/behjati/lr26/hg38/hg38.fa"
+ref_dir="/lustre/scratch126/cellgen/behjati/lr26/hg38"
+### change to reference ###
+cd "$ref_dir"
 
-# Indexing the T2T reference genome (if index files don't exist)
-pbmm2 index "$reference" "${reference%.fa}.mmi"
+### possibly decompress reference ###
+if [ ! -f "$reference" ]; then
+  gunzip -c "$reference_gz" > "$reference"
+fi
+
+### index the reference if not already done ###
+if [ ! -f "${reference%.fa}.mmi" ]; then
+    pbmm2 index "$reference" "${reference%.fa}.mmi"
+fi
+
