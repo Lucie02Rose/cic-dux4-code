799cb2e2bdc00d464ca8c92903b5df6ded81e7fa
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-28 11:15:46 +0100

    Update severus.sh

--- 4.structural_variants/SEVERUS/severus.sh
+++ 4.structural_variants/SEVERUS/severus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 16
 #BSUB -M 75000
 #BSUB -R 'span[hosts=1] select[mem>75000] rusage[mem=75000]'
@@ -8,22 +9,23 @@
 #BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-severus.out
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-severus.err
 
-# Activate conda environment for Severus
+# activate the severus conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/etc/profile.d/conda.sh
 conda activate severus_env
 
-# Directories
-reference="/nfs/users/nfs_l/lr26/nextflow_pipeline/reference/T2T/chm13v2.0.fa"
+### define the input, outpu and reference ###
+reference="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0.fa"
 input_tumor="/lustre/scratch126/cellgen/behjati/lr26/PacBio-aligned/tumor_all_4_hifi_reads_pbmm2.bam"
 input_blood="/lustre/scratch126/cellgen/behjati/lr26/PacBio-aligned/blood_1C01_hifi_reads_pbmm2.bam"
-output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-severus-new"
+output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-severus"
 
-# Create output directory if it does not exist
+### create the output directory ###
 mkdir -p "$output_dir"
 
-# Process each BAM file in the directory
+### print statement for processing ###
 echo "Running Severus..."
 
+### same settings as with the previous somatic callers ###
 severus \
    --target-bam "$input_tumor" \
    --control-bam "$input_blood" \
@@ -36,5 +38,6 @@ severus \
    --min-sv-size 20 \
    --single-bp \
    --output-read-ids
-
+   
+### pring statement for being finished ###
 echo "Severus SV call completed"
