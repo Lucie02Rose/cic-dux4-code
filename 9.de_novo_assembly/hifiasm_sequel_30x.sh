b7ff44575ce7b133e19293eba5cc30b2b0ea8395
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-28 15:19:19 +0100

    Update and rename hifiasm_sequel.sh to hifiasm_sequel_30x.sh

--- 9.de_novo_assembly/hifiasm_sequel_30x.sh
+++ 9.de_novo_assembly/hifiasm_sequel_30x.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 64
 #BSUB -M 400000
 #BSUB -R 'span[hosts=1] select[mem>400000] rusage[mem=400000]'
 #BSUB -q hugemem
-#BSUB -J hifitum
+#BSUB -J sequel_hifiasm
 #BSUB -G team274
 #BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-sequeldenovo.out
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-sequeldenovo.err
 
-### Activate conda environment
+### Acactivate the hifiasm conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate hifiasm-env
 
-# List of input FASTQ.gz files
+### concatenate and list the fastq files (this may already have been done in the alignment step) ###
 input_fastq1="/lustre/scratch126/cellgen/behjati/lr26/PacBio-fastq/tumor_1A01_hifi_reads.fastq.gz"
 input_fastq2="/lustre/scratch126/cellgen/behjati/lr26/PacBio-fastq/tumor_1A02_hifi_reads.fastq.gz"
 input_fastq3="/lustre/scratch126/cellgen/behjati/lr26/PacBio-fastq/tumor_2B01_hifi_reads.fastq.gz"
 combined_fastq="/lustre/scratch126/cellgen/behjati/lr26/PacBio-fastq/tumor_sequel_hifi_reads.fastq.gz"
-output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-denovo"
-tmp_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-denovo/tmp"
+output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-sequel"
+tmp_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-sequel/tmp"
 
-# Create temporary directory
+### create the output and temporary directory, export the temporary directory ####
 mkdir -p "$output_dir"
 mkdir -p "$tmp_dir"
 export TMPDIR="$tmp_dir"
 
-# Combine FASTQ.gz files
+### concatenate the sequel II runs to have one 30x depth of coverage file ###
 cat "$input_fastq1" "$input_fastq2" "$input_fastq3" > "$combined_fastq"
-
-# Now run hifiasm with combined reads
+### run hifiasm on the sequel II ###
 hifiasm -o "$output_dir" -t64 "$combined_fastq"
-
-echo "assembly process has started. Check logs in $output_dir"
+### note that assembly is running ###
+echo "assembly process has started"
