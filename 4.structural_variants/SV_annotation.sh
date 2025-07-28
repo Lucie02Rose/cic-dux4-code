44e644f8bc2ff73de524668c3b09c113103c7919
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-28 14:08:23 +0100

    Update SV_annotation.sh

--- 4.structural_variants/SV_annotation.sh
+++ 4.structural_variants/SV_annotation.sh
@@ -16,7 +16,7 @@ conda activate bioinfo
 ### define all the directories - before running this script I ensure that I have all the output samples named ###
 ### how I want them in the correct directories - this means that I have moverd them to the respective directories here ###
 ### which is vital for this step to work ###
-### this is also provided that sawfish filtering was carried out ###
+### this is also provided that sawfish filtering and sorting of nanomonsv was carried out ###
 ### if there are any problems with these steps, they can be fixed by consulting the output and error files and using bcftools to sanitize variants ###
 nanomon="/lustre/scratch126/cellgen/behjati/lr26/PacBio-nanomonsv/tumor-all.nanomonsv.result.sorted.vcf"
 nanomon_annot="/lustre/scratch126/cellgen/behjati/lr26/PacBio-nanomonsv/tumor-all.nanomonsv.result.annotated.vcf"
@@ -27,8 +27,11 @@ sawfish_repeats="/lustre/scratch126/cellgen/behjati/lr26/PacBio-sawfish/sawfish-
 savana="/lustre/scratch126/cellgen/behjati/lr26/PacBio-savana/savana.somatic.vcf"
 savana_annot="/lustre/scratch126/cellgen/behjati/lr26/PacBio-savana/savana.somatic.annotated.vcf"
 savana_repeats="/lustre/scratch126/cellgen/behjati/lr26/PacBio-savana/savana.somatic.annotated.repeats.vcf"
+### note that for severus, it has firstly been converted from the tsv ouput to a vcf using the variant_callers_filtering.ipynb script ###
 severus="/lustre/scratch126/cellgen/behjati/lr26/PacBio-severus/severus_somatic_breakpoints_double.vcf"
 severus_annot="/lustre/scratch126/cellgen/behjati/lr26/PacBio-severus/severus_somatic_breakpoints_double.annotated.vcf"
+severus_repeats="/lustre/scratch126/cellgen/behjati/lr26/PacBio-severus/severus_somatic_breakpoints_double.annotated.repeats.vcf"
+### files to use for annotation ###
 repeats="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0_RepeatMasker_4.1.2p1.2022Apr14.bed"
 gff3_gz="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0_RefSeq_Liftoff_v5.2.gff3.gz"
 gff3="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0_RefSeq_Liftoff_v5.2.gff3"
@@ -36,6 +39,7 @@ bed="/lustre/scratch126/cellgen/behjati/lr26/T2T/chm13v2.0_RefSeq_Liftoff_v5.2.b
 
 ### convert the gff3 to the bed format for annotation, provided it is sorted by chromosome, extract what is needed ###
 ### and directly sort the bed file by chromosome ###
+### repeat annotation from repeat masker does not really need annotation ###
 zcat "$gff3_gz" | awk -F'\t' '
 $3 ~ /^(gene|transcript|exon|CDS|five_prime_UTR|three_prime_UTR)$/ {
     id = "."  # default
@@ -51,35 +55,74 @@ bgzip -c "$bed" > "${bed}.gz"
 tabix -p bed "${bed}.gz"
 
 ### annotation steps for all the outputs ###
+### nanomonsv ###
+### bed file genic elements ###
 bcftools annotate \
-  -a "$repeats" \
-  -c CHROM,FROM,TO,INFO/repeats \
-  -h <(echo '##INFO=<ID=repeats,Number=1,Type=String,Description="Repeats from T2T annotation">') \
+  -a "$bed" \
+  -c CHROM,FROM,TO,INFO/genes \
+  -h <(echo '##INFO=<ID=genes,Number=1,Type=String,Description="Genes from T2T annotation">') \
   -o "$nanomon_annot" \
   -O z \
   "$nanomon"
-
+### bed file repeat masker ###
 bcftools annotate \
   -a "$repeats" \
   -c CHROM,FROM,TO,INFO/repeats \
   -h <(echo '##INFO=<ID=repeats,Number=1,Type=String,Description="Repeats from T2T annotation">') \
+  -o "$nanomon_repeats" \
+  -O z \
+  "$nanomon_annot"
+  
+### savana ###
+### bed file genic elements ###
+bcftools annotate \
+  -a "$bed" \
+  -c CHROM,FROM,TO,INFO/genes \
+  -h <(echo '##INFO=<ID=genes,Number=1,Type=String,Description="Genes from T2T annotation">') \
   -o "$savana_annot" \
   -O z \
   "$savana"
-
+### bed file repeat masker ###
 bcftools annotate \
   -a "$repeats" \
   -c CHROM,FROM,TO,INFO/repeats \
   -h <(echo '##INFO=<ID=repeats,Number=1,Type=String,Description="Repeats from T2T annotation">') \
+  -o "$savana_repeats" \
+  -O z \
+  "$savana_annot"
+  
+### severus ###
+### bed file genic elements ###
+bcftools annotate \
+  -a "$bed" \
+  -c CHROM,FROM,TO,INFO/genes \
+  -h <(echo '##INFO=<ID=genes,Number=1,Type=String,Description="Genes from T2T annotation">') \
   -o "$severus_annot" \
   -O z \
   "$severus"
-
+### bed file repeat masker ###
 bcftools annotate \
   -a "$repeats" \
   -c CHROM,FROM,TO,INFO/repeats \
   -h <(echo '##INFO=<ID=repeats,Number=1,Type=String,Description="Repeats from T2T annotation">') \
+  -o "$severus_repeats" \
+  -O z \
+  "$severus_annot"
+  
+### sawfish ###
+### bed file genic elements ###
+bcftools annotate \
+  -a "$bed" \
+  -c CHROM,FROM,TO,INFO/genes \
+  -h <(echo '##INFO=<ID=genes,Number=1,Type=String,Description="Genes from T2T annotation">') \
   -o "$sawfish_annot" \
   -O z \
   "$sawfish"
-
+### bed file repeat masker ###
+bcftools annotate \
+  -a "$repeats" \
+  -c CHROM,FROM,TO,INFO/repeats \
+  -h <(echo '##INFO=<ID=repeats,Number=1,Type=String,Description="Repeats from T2T annotation">') \
+  -o "$sawfish_repeats" \
+  -O z \
+  "$sawfish_annot"
