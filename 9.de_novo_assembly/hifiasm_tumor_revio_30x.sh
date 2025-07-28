1560e4034de17e5980098c0ac42e50048ce01c2f
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-28 15:27:23 +0100

    Update hifiasm_tumor_revio_30x.sh

--- 9.de_novo_assembly/hifiasm_tumor_revio_30x.sh
+++ 9.de_novo_assembly/hifiasm_tumor_revio_30x.sh
@@ -5,7 +5,7 @@
 #BSUB -q hugemem
 #BSUB -J tumor_hifi_revio
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-tumor_hifi_revioo.out
+#BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-tumor_hifi_revio.out
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-tumor_hifi_revio.err
 
 ### activate the hifiasm conda environment ###
@@ -17,12 +17,12 @@ combined_fastq="/lustre/scratch126/cellgen/behjati/lr26/PacBio-fastq/tumor_1B01_
 output_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-revio"
 tmp_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-revio/tmp"
 
-### create the output and temporary directory, export the temporary directory ####
+### create the output and temporary directory, export the temporary directory ###
 mkdir -p "$output_dir"
 mkdir -p "$tmp_dir"
 export TMPDIR="$tmp_dir"
 
-### run hifiasm on the sequel II ###
+### run hifiasm on the revio ###
 hifiasm -o "$output_dir" -t64 "$combined_fastq"
 ### note that assembly is running ###
 echo "assembly process has started"
