e93baba8f5d04356e4c96b618e0829d97515c939
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-07-28 15:43:50 +0100

    Update hifiasm-sequel-conversion.sh

--- 9.de_novo_assembly/hifiasm-sequel-conversion.sh
+++ 9.de_novo_assembly/hifiasm-sequel-conversion.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 16
-#BSUB -M 32000
-#BSUB -R 'span[hosts=1] select[mem>32000] rusage[mem=32000]'
+#BSUB -M 1000
+#BSUB -R 'span[hosts=1] select[mem>1000] rusage[mem=1000]'
 #BSUB -q yesterday
 #BSUB -J pbmm2-hifiasm
 #BSUB -G team274
 #BSUB -o /lustre/scratch126/cellgen/behjati/lr26/outputs/%J-giabhificon.out
 #BSUB -e /lustre/scratch126/cellgen/behjati/lr26/errors/%J-giabhificon.err
 
-### Activate conda environment
+### activate the base conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate base
 
-# Directories
+### directories for the conversion 
 reference="/nfs/users/nfs_l/lr26/nextflow_pipeline/reference/T2T/chm13v2.0.mmi"
 giab_dir="/lustre/scratch126/cellgen/behjati/lr26/PacBio-denovo"
 
