9c1e608af58921c41799a5a24cd1cb87dae73670
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-04 16:47:45 +0100

    Update deepvariant-blood.sh

--- 7.small_variant_calling/deepvariant-blood.sh
+++ 7.small_variant_calling/deepvariant-blood.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 32
 #BSUB -M 100000
 #BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
 #BSUB -q long
-#BSUB -J pepper
+#BSUB -J dmp
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/%J.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/%J.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-deepvar-blood.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-deepvar-blood.err
+### note that this script was used pre-farmageddon (hence casm/team274sb)
 
-# Load Singularity module
+### load teh farm singulatiry module ###
 module load ISG/singularity/3.11.4
 
-# Set cache and temp directories (modify paths as needed)
+### set and export caches for the singulaty ###
 export SINGULARITY_CACHEDIR=/lustre/scratch126/casm/team274sb/lr26/singularity
 export SINGULARITY_TMPDIR=/lustre/scratch126/casm/team274sb/lr26/singularity/tmp
 
-# Define paths
+### define the paths used by the container ### 
 SINGULARITY_IMG="/lustre/scratch126/casm/team274sb/lr26/singularity/pepper_margin_deepvariant_r0.8.sif"
-BAM_FILE="/lustre/scratch126/casm/team274sb/lr26/pbmm2-alignment/1_C01-revio/m84047_240202_155616_s3.hifi_reads.bc2026_pbmm2.bam"
+BAM_FILE="/lustre/scratch126/casm/team274sb/lr26/PacBio-aligned/blood_1C01_hifi_reads.bam"
 REFERENCE="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0.fa"
-OUTPUT_DIR="/lustre/scratch126/casm/team274sb/lr26/pepper-blood"
+OUTPUT_DIR="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-blood"
 
-# Ensure output directory exists
+### make the output directory ###
 mkdir -p $OUTPUT_DIR
 
-# Run PEPPER-Margin-DeepVariant with Singularity
+### run the pepper margin deepvariant container with singularity according to instructions on their github ###
 singularity exec --bind /lustre/scratch126/casm/team274sb/lr26:/mnt \
     "${SINGULARITY_IMG}" \
     run_pepper_margin_deepvariant call_variant \
-    -b "/mnt/pbmm2-alignment/1_C01-revio/m84047_240202_155616_s3.hifi_reads.bc2026_pbmm2.bam" \
+    -b "/mnt/PacBio-aligned/blood_1C01_hifi_reads.bam" \
     -f "/mnt/T2T/chm13v2.0.fa" \
-    -o "/mnt/pepper-blood" \
+    -o "/mnt/PacBio-deepvariant-blood" \
     -p "blood_output" \
     -t 32 \
     --hifi
