aae9571112565a841615e7f60ba2a9b17abbfed4
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-04 19:59:19 +0100

    Update vep-deepvar-mom-fin38.sh

--- 7.small_variant_calling/vep-deepvar-mom-fin38.sh
+++ 7.small_variant_calling/vep-deepvar-mom-fin38.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
+### parameters for the lsf job ###
 #BSUB -n 16
 #BSUB -M 100000
 #BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
-#BSUB -q long
-#BSUB -J pepper-mom
+#BSUB -q basement
+#BSUB -J vep-mom
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/vepmom.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/vepmom.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-vep-mom-hg38.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-vep-mom-hg38.err
 
+### load the correct compiler to work with vep ###
 export PATH=/usr/bin:$PATH
 export PATH=$PATH:/nfs/users/nfs_l/lr26/ensembl-vep
 
-# Define pathways
+### input, vep and reference ###
 vep="/nfs/users/nfs_l/lr26/ensembl-vep"
-reference="/lustre/scratch126/casm/team274sb/lr26/hg38/genome.fa"
-input_vcf="/lustre/scratch126/casm/team274sb/lr26/pepper-mom38/mom_output.vcf.gz"
+reference="/lustre/scratch126/casm/team274sb/lr26/hg38/hg38.fa"
+input_vcf="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-mom-hg38/mom_output_norm.vcf.gz"
+### files to annotate with ###
 dbsnp="/lustre/scratch126/casm/team274sb/lr26/hg38/Homo_sapiens_assembly38.dbsnp138.vcf.gz"
-cosmic="/lustre/scratch126/casm/team274sb/lr26/hg38/cleaned_cosmic_hg38_nochr.vcf.gz"
 gff="/lustre/scratch126/casm/team274sb/lr26/hg38/gencode.sorted.gff3.gz"
 clinvar="/lustre/scratch126/casm/team274sb/lr26/hg38/clinvar.vcf.gz"
-output_dir="/lustre/scratch126/casm/team274sb/lr26/pepper-mom38"
+### output ###
+output_dir="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-mom-hg38"
 
-# Change to pepper
+### change to the output directory ###
 cd "$output_dir"
 
-# Run vep with custom ClinVar and dbSNP liftover annotations
-vep -i "$input_vcf" \
-    -o "$output_dir/mom_vep_annotated_cosmic_clinvar_dbsnp.vcf" \
-    --fasta "$reference" \
-    --gff "$gff" \
-    --species homo_sapiens \
-    --assembly GRCh38 \
-    --vcf \
-    --custom "$dbsnp",dbSNP,vcf,exact,0,ID \
-    --custom "$clinvar",ClinVar,vcf,overlap,0,CLNSIG \
-    --custom "$cosmic",cosmic,vcf,exact,0,COSMIC_ID \
-    --force_overwrite
+### run vep with custom ClinVar and dbSNP liftover annotations ###
+vep \
+  --i "$input_vcf" \
+  --fasta "$reference" \
+  --gff "$gff" \
+  --species homo_sapiens \
+  --assembly GRCh38 \
+  --custom "$clinvar",ClinVar,vcf,overlap,0,CLNSIG \
+  --custom "$dbsnp",dbSNP,vcf,exact,0,ID \
+  --o "$output_dir/mom_vep_annotated_with_clinvar_and_dbsnp_vcf_new_hg38.vcf" \
+  --vcf \
+  --force_overwrite
