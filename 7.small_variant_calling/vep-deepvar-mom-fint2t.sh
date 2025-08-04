519fb56d8b8e2cd5deedf439ce2178614ce0a997
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-04 18:31:38 +0100

    Update vep-deepvar-mom-fint2t.sh

--- 7.small_variant_calling/vep-deepvar-mom-fint2t.sh
+++ 7.small_variant_calling/vep-deepvar-mom-fint2t.sh
@@ -41,19 +41,6 @@ vep \
   --force_overwrite
 
 
-#!/bin/bash
-#BSUB -n 16
-#BSUB -M 100000
-#BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
-#BSUB -q long
-#BSUB -J pepper-mom
-#BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/vepmomt.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/vepmomt.err
-
-export PATH=/usr/bin:$PATH
-export PATH=$PATH:/nfs/users/nfs_l/lr26/ensembl-vep
-
 # Define pathways
 vep="/nfs/users/nfs_l/lr26/ensembl-vep"
 reference="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0.fa"
@@ -63,9 +50,6 @@ gff="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0_RefSeq_Liftoff_v5.1.so
 cosmic="/lustre/scratch126/casm/team274sb/lr26/T2T/Cosmic_MutantCensus_sorted_T2T.vcf.gz"
 clinvar="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0_ClinVar20220313.vcf.gz"
 output_dir="/lustre/scratch126/casm/team274sb/lr26/pepper-mom"
-
-# Change to pepper
-cd "$output_dir"
 
 # Run vep with custom ClinVar and dbSNP liftover annotations
 vep \
