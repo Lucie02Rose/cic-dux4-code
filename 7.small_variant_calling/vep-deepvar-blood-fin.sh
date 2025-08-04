04ba0e77ae4e0c18dfee982bb2dd9476d53614f9
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-04 18:29:49 +0100

    Update vep-deepvar-blood-fin.sh

--- 7.small_variant_calling/vep-deepvar-blood-fin.sh
+++ 7.small_variant_calling/vep-deepvar-blood-fin.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
+### parameters for the lsf job ###
 #BSUB -n 16
 #BSUB -M 100000
 #BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
 #BSUB -q basement
 #BSUB -J pepper-blood
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/pepperblood.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/pepperblood.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-vep-blood.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-vep-blood.err
 
-# Load correct compiler
+### load the correct compiler to work with vep ###
 export PATH=/usr/bin:$PATH
 export PATH=$PATH:/nfs/users/nfs_l/lr26/ensembl-vep
 
-# Define pathways
+### input, vep and reference ###
 vep="/nfs/users/nfs_l/lr26/ensembl-vep"
 reference="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0.fa"
-input_vcf="/lustre/scratch126/casm/team274sb/lr26/pepper-blood/blood_output.vcf.gz"
+input_vcf="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-blood/blood_output_norm.vcf.gz"
+### files to annotate with ###
 dbsnp="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0_dbSNPv155.vcf.gz"
 gff="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0_RefSeq_Liftoff_v5.1.sorted.gff3.gz"
 clinvar="/lustre/scratch126/casm/team274sb/lr26/T2T/chm13v2.0_ClinVar20220313.vcf.gz"
-output_dir="/lustre/scratch126/casm/team274sb/lr26/pepper-blood"
+### output ###
+output_dir="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-blood"
 
-# Change to pepper
+### change to the output directory ###
 cd "$output_dir"
 
-# Run vep with custom ClinVar and dbSNP liftover annotations
+### run vep with custom ClinVar and dbSNP liftover annotations ###
 vep \
   --input_file "$input_vcf" \
   --fasta "$reference" \
