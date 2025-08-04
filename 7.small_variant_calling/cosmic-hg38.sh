a5be7193a16dde669d11c6e1f65ffa0c0803b562
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-04 18:02:46 +0100

    Update cosmic-hg38.sh

--- 7.small_variant_calling/cosmic-hg38.sh
+++ 7.small_variant_calling/cosmic-hg38.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+### parameters for the lsf job ###
 #BSUB -n 16
 #BSUB -M 100000
 #BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
 #BSUB -q normal
 #BSUB -J cosmic
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/cos38%J.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/cos38%J.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-cosmic.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-cosmic.err
 
-### Activate Conda Environment
+### activate the bioinfo conda environment with bcftools ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate bioinfo
 
-### Directories
+### define the directories (COSMIC vcf is from the database) ###
 cosmic="/lustre/scratch126/casm/team274sb/lr26/hg38/cleaned_cosmic_hg38.vcf.gz"
-reference="/lustre/scratch126/casm/team274sb/lr26/hg38/genome.fa"
-mom="/lustre/scratch126/casm/team274sb/lr26/pepper-mom38/mom_output.vcf.gz"
-blood="/lustre/scratch126/casm/team274sb/lr26/pepper-blood38/blood_output.vcf.gz"
-tumor="/lustre/scratch126/casm/team274sb/lr26/pepper-tumor38/tumor_output.vcf.gz"
-dir="/lustre/scratch126/casm/team274sb/lr26/pepper-tumor38"
+reference="/lustre/scratch126/casm/team274sb/lr26/hg38/hg38.fa"
+mom="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-mom-hg38/mom_output.vcf.gz"
+blood="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-blood-hg38/blood_output.vcf.gz"
+tumor="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-tumor-hg38/tumor_output.vcf.gz"
+dir="/lustre/scratch126/casm/team274sb/lr26/PacBio-deepvariant-tumor-hg38"
 
 cd "$dir"
 
@@ -35,19 +36,19 @@ tabix -p vcf normalized_cosmic_hg38.vcf.gz
 bcftools isec -p isec_tumor_germline -C normalized_tumor_output.vcf.gz normalized_blood_output.vcf.gz normalized_mom_output.vcf.gz
 bcftools isec -p isec_blood_mom_somatic -n=2 normalized_blood_output.vcf.gz normalized_mom_output.vcf.gz
 
-#bcftools annotate \
- #   -a "$cosmic" \
-  #  -c CHROM,POS,INFO \
-   # -o annotated_tumor_somatic_new38.vcf.gz \
-   # -O z \
-   # tumor_somatic_hg38.vcf.gz
+bcftools annotate \
+    -a "$cosmic" \
+    -c CHROM,POS,INFO \
+    -o annotated_tumor_somatic_new38.vcf.gz \
+    -O z \
+    tumor_somatic_hg38.vcf.gz
 
-#bcftools annotate \
- #   -a "$cosmic" \
-  #  -c CHROM,POS,INFO \
-   # -o annotated_shared_germline_new38.vcf.gz \
-   # -O z \
-   # blood_mom_somatic_hg38.vcf.gz
+bcftools annotate \
+    -a "$cosmic" \
+    -c CHROM,POS,INFO \
+    -o annotated_shared_germline_new38.vcf.gz \
+    -O z \
+    blood_mom_somatic_hg38.vcf.gz
 
 
 
