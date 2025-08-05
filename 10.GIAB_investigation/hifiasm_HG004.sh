9791082de021273ba05815f2102057bc1a5989a0
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 14:35:37 +0100

    Update hifiasm_HG004.sh

--- 10.GIAB_investigation/hifiasm_HG004.sh
+++ 10.GIAB_investigation/hifiasm_HG004.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 64
-#BSUB -M 400000
-#BSUB -R 'span[hosts=1] select[mem>400000] rusage[mem=400000]'
+#BSUB -M 200000
+#BSUB -R 'span[hosts=1] select[mem>200000] rusage[mem=200000]'
 #BSUB -q hugemem
-#BSUB -J hifitum
+#BSUB -J hg004
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/hg004.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/hg004.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-hg004.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-hg004.err
 
-### Activate conda environment
+### activate the hifiasm conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate hifiasm-env
 
 # Define directories
-input_fastq="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/fastq-revio-sprq/m84039_241002_020632_s4.hifi_reads.bc2021.fastq.gz"
-output_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG004_denovo_hifiasm"
-tmp_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG004_denovo_hifiasm/denovhg004"
+input_fastq="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG004-rep1.fastq"
+output_dir="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG004_revio"
+tmp_dir="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG004_revio/denovhg004"
 
-# Create temporary directory
+### create the output and temporary directory, export the temporary directory ####
 mkdir -p "$output_dir"
 mkdir -p "$tmp_dir"
 export TMPDIR="$tmp_dir"
 
-# Run Flye with reduced memory & threads
+### run hifiasm ###
 hifiasm -o "$output_dir" -t64 "$input_fastq"
-
-echo "Flye assembly process has started. Check logs in $output_dir"
-
+### note that assembly is running ###
+echo "Assembly process has started. Check logs in $output_dir"
