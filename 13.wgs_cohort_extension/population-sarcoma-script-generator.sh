c633051e098c68e2ce9eb2b688a3e5dd2e9e83d2
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 09:53:54 +0100

    Update and rename population-dux-master.sh to population-sarcoma-script-generator.sh

--- 13.wgs_cohort_extension/population-sarcoma-script-generator.sh
+++ 13.wgs_cohort_extension/population-sarcoma-script-generator.sh
@@ -1,13 +1,13 @@
-# Base template
+### base of template script ###
 template_script="population-dux.sh"
 
-# List of sarcoma_normal samples
+### list of all sarcoma normal samples ###
 sarcoma_normals=(
     "PD41847b" "PD41848b" "PD42181b" "PD46692b" "PD46696b" "PD46697b"
     "PD46698b" "PD47706b" "PD47708b" "PD51353b" "PD54858b" "PD54859b"
 )
 
-# Generate a new script per sample
+### generate a new script using the sample ID for each replacing all instances of the PD54859b ###
 for sample in "${sarcoma_normals[@]}"; do
     new_script="script_${sample}.sh"
 
