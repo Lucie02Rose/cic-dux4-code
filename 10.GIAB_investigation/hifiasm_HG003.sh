32ca85559f1b6c8d8ac844407ee7267fd4c0df23
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 14:35:06 +0100

    Update hifiasm_HG003.sh

--- 10.GIAB_investigation/hifiasm_HG003.sh
+++ 10.GIAB_investigation/hifiasm_HG003.sh
@@ -4,10 +4,10 @@
 #BSUB -M 200000
 #BSUB -R 'span[hosts=1] select[mem>200000] rusage[mem=200000]'
 #BSUB -q hugemem
-#BSUB -J hg002
+#BSUB -J hg003
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-hg002.out
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-hg002.err
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-hg003.out
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-hg003.err
 
 ### activate the hifiasm conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
