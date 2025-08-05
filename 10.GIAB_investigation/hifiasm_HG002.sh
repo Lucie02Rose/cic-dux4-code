ba6a284662cd353d872a6eeea8008e99cbd51a32
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 13:46:43 +0100

    Update hifiasm_HG002.sh

--- 10.GIAB_investigation/hifiasm_HG002.sh
+++ 10.GIAB_investigation/hifiasm_HG002.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+### parameters for the LSF job ###
 #BSUB -n 64
-#BSUB -M 400000
-#BSUB -R 'span[hosts=1] select[mem>400000] rusage[mem=400000]'
+#BSUB -M 200000
+#BSUB -R 'span[hosts=1] select[mem>200000] rusage[mem=200000]'
 #BSUB -q hugemem
 #BSUB -J hifitum
 #BSUB -G team274
 #BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/hg002.out
 #BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/hg002.err
 
-### Activate conda environment
+### activate the hifiasm conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate hifiasm-env
 
@@ -17,13 +18,13 @@ input_fastq="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/fastq-revio
 output_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG002_denovo_hifiasm"
 tmp_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG002_denovo_hifiasm/denovhg002"
 
-# Create temporary directory
+### create the output and temporary directory, export the temporary directory ####
 mkdir -p "$output_dir"
 mkdir -p "$tmp_dir"
 export TMPDIR="$tmp_dir"
 
-# Run Flye with reduced memory & threads
+### run hifiasm ###
 hifiasm -o "$output_dir" -t64 "$input_fastq"
-
-echo "Flye assembly process has started. Check logs in $output_dir"
+### note that assembly is running ###
+echo "Assembly process has started. Check logs in $output_dir"
 
