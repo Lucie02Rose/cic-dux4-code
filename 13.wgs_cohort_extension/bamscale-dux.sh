7b3a9d173f3678a1be425099f31d9b16aaf5e760
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 10:12:53 +0100

    Update bamscale-dux.sh

--- 13.wgs_cohort_extension/bamscale-dux.sh
+++ 13.wgs_cohort_extension/bamscale-dux.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
+### parameters for the lsf job ###
 #BSUB -n 40
 #BSUB -M 100000
 #BSUB -R 'span[hosts=1] select[mem>100000] rusage[mem=100000]'
 #BSUB -q normal
-#BSUB -J t2t-alignment-single
+#BSUB -J bamscale
 #BSUB -G team274
-#BSUB -o /lustre/scratch126/casm/team274sb/lr26/output_logs/bamscale.%J.o
-#BSUB -e /lustre/scratch126/casm/team274sb/lr26/error_logs/bamscale.%J.e
+#BSUB -o /lustre/scratch126/casm/team274sb/lr26/outputs/%J-bamscale.o
+#BSUB -e /lustre/scratch126/casm/team274sb/lr26/errors/%J-bamscale.e
 
-set -euo pipefail
-
-echo "Activating environment"
+### activate the bamscale conda environment ###
 source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate bamscale
 
-# Set parameters
-threads=40
+### define bam scale parameters - dux coordinates have been saved in a bed file ###
 roi_bed="/lustre/scratch126/casm/team274sb/lr26/population_dux/dux.bed"
 output_dir="/lustre/scratch126/casm/team274sb/lr26/population_dux"
 bam_dir="$output_dir"
 bam_out="$output_dir/bamscale_output"
-
+### make the output directory ###
 mkdir -p "$bam_out"
 
-# Run BAMscale in parallel (1 thread per BAM, 40 BAMs at a time)
-ls "$bam_dir"/*.bam | parallel -j $threads '
+### run bamscale in parallel 1 per core ###
+ls "$bam_dir"/*.bam | parallel -j 40 '
   sample=$(basename {} .bam);
   echo "Processing $sample";
   BAMscale cov --bed "'$roi_bed'" \
