cbd2ef1ae3fcc106375d5a35898bcf4eece3e52c
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 14:20:47 +0100

    Update hifiasm_HG002.sh

--- 10.GIAB_investigation/hifiasm_HG002.sh
+++ 10.GIAB_investigation/hifiasm_HG002.sh
@@ -14,9 +14,9 @@ source /software/cellgen/team274/lr26/miniforge3/bin/activate
 conda activate hifiasm-env
 
 # Define directories
-input_fastq="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/fastq-revio-sprq/m84039_241001_220042_s2.hifi_reads.bc2018.fastq.gz"
-output_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG002_denovo_hifiasm"
-tmp_dir="/lustre/scratch126/casm/team274sb/lr26/pbmm2-giab-revio/HG002_denovo_hifiasm/denovhg002"
+input_fastq="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG002-rep1.fastq"
+output_dir="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG002_revio"
+tmp_dir="/lustre/scratch126/casm/team274sb/lr26/GIAB/HG002_revio/denovhg002"
 
 ### create the output and temporary directory, export the temporary directory ####
 mkdir -p "$output_dir"
@@ -27,4 +27,3 @@ export TMPDIR="$tmp_dir"
 hifiasm -o "$output_dir" -t64 "$input_fastq"
 ### note that assembly is running ###
 echo "Assembly process has started. Check logs in $output_dir"
-
