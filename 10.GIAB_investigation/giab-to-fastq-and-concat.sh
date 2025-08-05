4055413dc8b38e8a00d79a2d8d1c975eba877681
Lucie02Rose <111454610+Lucie02Rose@users.noreply.github.com>
2025-08-05 14:18:35 +0100

    Update and rename giab-to-fasta-and-concat.sh to giab-to-fastq-and-concat.sh

--- 10.GIAB_investigation/giab-to-fastq-and-concat.sh
+++ 10.GIAB_investigation/giab-to-fastq-and-concat.sh
@@ -22,37 +22,37 @@ cd "$giab_dir"
 
 ### function to convert bam files to fasta using new samtools ###
 ### defining file names and running ###
-convert_bam_to_fasta() {
+convert_bam_to_fastq() {
     bamfile="$1"
     base=$(basename "$bamfile" .bam)
-    fasta_out="$giab_dir/${base}.fasta"
-    echo "Converting $bamfile to $fasta_out"
-    samtools fasta "$bamfile" > "$fasta_out"
+    fastq_out="$giab_dir/${base}.fastq"
+    echo "Converting $bamfile to $fastq_out"
+    samtools fastq "$bamfile" > "$fastq_out"
 }
 ### exporting and running the function ###
-export -f convert_bam_to_fasta
+export -f convert_bam_to_fastq
 export giab_dir
 
 ### find the corresponding bam files in the directory and use the function in parallel ###
-find "$giab_dir" -name "*HG00?-rep?.bam" | parallel -j 8 convert_bam_to_fasta
-find "$giab_dir" -name "*HG00?_sprq.bam" | parallel -j 8 convert_bam_to_fasta
+find "$giab_dir" -name "*HG00?-rep1.bam" | parallel -j 8 convert_bam_to_fastq
+find "$giab_dir" -name "*HG00?_sprq.bam" | parallel -j 8 convert_bam_to_fastq
 
 ### combine the revio and sprq together if both present ###
 ### error handling for each individual ###
-if [[ -f HG002-rep1.fasta && -f HG002_sprq.fasta ]]; then
-    cat HG002-rep1.fasta HG002_sprq.fasta > combined_HG002.fasta
+if [[ -f HG002-rep1.fastq && -f HG002_sprq.fastq ]]; then
+    cat HG002-rep1.fastq HG002_sprq.fastq > combined_HG002.fastq
 else
-    echo "WARNING: Missing FASTA(s) for HG002. Skipping concatenation."
+    echo "missing both fastqs, skipping."
 fi
 
-if [[ -f HG003-rep1.fasta && -f HG003_sprq.fasta ]]; then
-    cat HG003-rep1.fasta HG003_sprq.fasta > combined_HG003.fasta
+if [[ -f HG003-rep1.fastq && -f HG003_sprq.fastq ]]; then
+    cat HG003-rep1.fastq HG003_sprq.fastq > combined_HG003.fastq
 else
-    echo "WARNING: Missing FASTA(s) for HG003. Skipping concatenation."
+    echo "missing both fastqs, skipping."
 fi
 
-if [[ -f HG004-rep1.fasta && -f HG004_sprq.fasta ]]; then
-    cat HG004-rep1.fasta HG004_sprq.fasta > combined_HG004.fasta
+if [[ -f HG004-rep1.fastq && -f HG004_sprq.fastq ]]; then
+    cat HG004-rep1.fastq HG004_sprq.fastq > combined_HG004.fastq
 else
-    echo "WARNING: Missing FASTA(s) for HG004. Skipping concatenation."
+    echo "missing both fastqs, skipping."
 fi
